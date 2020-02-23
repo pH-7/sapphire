8a6b76b291d4af82423c2e6ed40f900db4b951fa
Pierre-Henry Soria <hi@ph7.me>
2020-02-24 06:53:06 +1030

    IMPLEMENT API routes

---
+++ src/routes.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+use FastRoute\RouteCollector;
+use Sapphire\App\Http\Action\Product\GetItemDetails;
+use Sapphire\App\Http\Action\Product\GetItems;
+use Sapphire\App\Http\Action\Utility\StaticFiles;
+use function FastRoute\simpleDispatcher;
+
+return simpleDispatcher(static function (RouteCollector $routes) use ($products, $orders) {
+    $routes->get('/products', GetItems::class);
+    $routes->get('/product/{id:\d+}', GetItemDetails::class);
+    $routes->get('/static/{file:.*\.\w+}', Sta
+    ticFiles::class);
+});
